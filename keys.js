4e578a7d5fb038b31db041b03ba3f15e01e22c01
Jose Araujo <JoseAraujoBH@gmail.com>
2018-07-23 16:19:34 -0400

    final push

--- keys.js
+++ keys.js
@@ -1,13 +1,13 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: 'Rf580oM5JoChFgI0bSDrbdGhF',
+  consumer_secret: 'pF9AKYXSoIPpwnd9rZ7LkfEx8YqYS74QZnXTit7YuoDQIS7zl5',
+  access_token_key: '983803398842650625-PJ8zooa6tw5v17180KA1k1MaO7Xw5w3',
+  access_token_secret: 'sw5ycYh6TgqbYchrK33tlKORDEaa2cvbpWq6AYZ7ZoZ3E'
 };
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: 'b4eb75acb0c7477c8cc160def9af1610',
+  secret: '57b00df29b9c47ecb8b0f1ab81eb268e'
 };
